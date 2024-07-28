8e880620dbc4ee19045ec1d1d87c852c5f4e4fa2
Dynastry25 <81444153+Dynastry25@users.noreply.github.com>
2024-07-28 18:07:44 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,11 +3,11 @@ const chalk = require('chalk');
 require('dotenv').config();
 
 //contact details
-global.ownernumber = [ process.env.OWNER_NUMBER || "919931122319"],
-global.ownername = process.env.OWNER_NAME || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.ownernumber = [ process.env.OWNER_NUMBER || "255753266350"],
+global.ownername = process.env.OWNER_NAME || "Dynastry",
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.location = "Tanzania"
 
 global.botname = process.env.BOTNAME || "𝐌𝐀𝐑𝐈𝐀-𝐌𝐃",//name of the bot
 
